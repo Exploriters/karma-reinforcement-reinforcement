65c88c3f80a73e8c3c592c396c055a6ac278f308
Magnus siiftun1857 Frankline <39226927@qq.com>
2023-05-01 20:47:55 +0800

    初始化

---
+++ src/OptionsMenu.cs
@@ -0,0 +1,25 @@
+﻿using Menu.Remix.MixedUI;
+
+namespace Explorite.KarmaReinforcementReinforcement
+{
+	class OptionsMenu : OptionInterface
+	{
+		public Configurable<bool> KarmaReinforcementEnabled { get; }
+		public OptionsMenu(Plugin plugin)
+		{
+			KarmaReinforcementEnabled = config.Bind<bool>("KARMAREINFORCEMENTREINFORCEMENT", false);
+		}
+		public override void Initialize()
+		{
+			var opTab = new OpTab(this, "Tab"); //Create a tab.
+			this.Tabs = new OpTab[] {
+				opTab //Add the tab into your list of tabs.
+            };
+			UIelement[] UIarrayOptions = new UIelement[] { //create an array of ui elements
+				new OpLabel(40f, 550f, "Karma reinforcement reinforcement", true), //Creates a label at 10,550 with big text saying "Options"
+				new OpCheckBox(KarmaReinforcementEnabled, new UnityEngine.Vector2(10f, 550f)),
+			};
+			opTab.AddItems(UIarrayOptions); //adds the elements to the tab
+		}
+	}
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using SlugBase.Features;
+using static SlugBase.Features.FeatureTypes;
+using Menu;
+
+namespace Explorite.KarmaReinforcementReinforcement
+{
+	[BepInPlugin(MOD_ID, "Slugcat Template", "0.1.0")]
+	class Plugin : BaseUnityPlugin
+	{
+		private const string MOD_ID = "explorite.karma_reinforcement_reinforcement";
+
+		//public static readonly GameFeature<bool> PrementKarmaReinforcement = GameBool("explorite/prement_karma_reinforcement");
+		private OptionsMenu options { get; }
+		public Plugin()
+		{
+			options = new OptionsMenu(this);
+		}
+
+
+		// Add hooks
+		public void OnEnable()
+		{
+			On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+			On.RainWorld.OnModsInit += Hook_OnModsInit;
+
+			// Put your custom hooks here!
+			On.OverWorld.Update += OverWorld_Update;
+			On.OverWorld.LoadWorld;
+		}
+
+		// Load any resources, such as sprites or sounds
+		private void LoadResources(RainWorld rainWorld)
+		{
+		}
+		public void Hook_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+		{
+			orig(self);
+			try
+			{
+				MachineConnector.SetRegisteredOI(MOD_ID, options);
+			}
+			catch (Exception)
+			{
+				/* make sure to error-proof your hook, 
+				otherwise the game may break 
+				in a hard-to-track way
+				and other mods may stop working */
+			}
+		}
+
+		// Implement Karma
+		private void OverWorld_Update(On.OverWorld.orig_Update orig, OverWorld self)
+		{
+			orig(self);
+
+			if (
+				//PrementKarmaReinforcement.TryGet(self.game, out bool enabled)
+				//&& enabled
+				//&& 
+				options.KarmaReinforcementEnabled.Value &&
+				!(self.game.session as StoryGameSession).saveState.deathPersistentSaveData.reinforcedKarma
+				)
+			{
+				(self.game.session as StoryGameSession).saveState.deathPersistentSaveData.reinforcedKarma = true;
+			}
+		}
+	}
+}
